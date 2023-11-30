37e17ea35c100a839e9ce9308d4766b41ded1ec6
Ian Chamba <86791147+ianchamba@users.noreply.github.com>
2023-11-30 15:30:28 -0300

    Create stock-manager-by-user-role.php

---
+++ stock-manager-by-user-role.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Stock Manager By User Role
+Description: Um plugin simples para gerenciar estoque com base nas funções do usuário.
+Version: 1.0
+Author: Ian Chamba
+Author URI: https://chamba.company
+*/
+
+// Inclui o arquivo functions.php
+include_once(plugin_dir_path(__FILE__) . 'functions.php');
